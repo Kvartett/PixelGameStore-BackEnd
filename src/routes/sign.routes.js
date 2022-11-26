6c9c2ef4cc9c8c300749b3ba92fe758625e292b5
Luiz <felipedevluiz@gmail.com>
2022-11-26 14:58:09 -0300

    feat: get games HomePage

--- src/routes/sign.routes.js
+++ src/routes/sign.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express"
 import { signUp, signIn } from "../controllers/sign.controllers.js"
+import { getHomePage } from "../controllers/home.controllers.js"
+import { validateToken } from "../middleware/validate.token.middleware.js"
 
 const router = Router()
 
 router.post("/auth/sign-up", signUp)
 router.post("/auth/sign-in", signIn)
+router.post("/", getHomePage)
 
 export default router
